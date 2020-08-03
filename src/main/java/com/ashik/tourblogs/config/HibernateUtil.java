09a5fae7844357a984d8d84f1c66414cac10d543
pranto <ashikmdshakilpranto@gmail.com>
2020-08-03 13:19:43 +0600

    sdas

--- src/main/java/com/ashik/tourblogs/config/HibernateUtil.java
+++ src/main/java/com/ashik/tourblogs/config/HibernateUtil.java
@@ -7,7 +7,7 @@ import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
 	public static SessionFactory getSessionFactory() {
-		Configuration config = new Configuration().configure("hibernate.cfg.xml")
+		Configuration config = new Configuration().configure("myhibernate.cfg.xml")
 				.addAnnotatedClass(com.ashik.tourblogs.entities.Blogger.class)
 				.addAnnotatedClass(com.ashik.tourblogs.entities.Blog.class)
 				.addAnnotatedClass(com.ashik.tourblogs.entities.Comment.class);
